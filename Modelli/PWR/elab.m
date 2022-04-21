48b0cc3b4886f317419ec00af3d720ff6fa8d9d3
Lorenzo Mazzocco <lorenzo.mazzocco01@gmail.com>
2022-04-21 16:32:05 +0200

    avanzato molto il data processing dello steady state PWR

---
+++ Modelli/PWR/elab.m
@@ -0,0 +1,73 @@
+data = readtable('data.csv');
+
+time = data.time;
+power = data.cntrlvar110;
+T_out = data.tempf555500000;
+
+T_fuel_max_do = data.httemp555000101;
+T_fuel_max_med = data.httemp555002501;
+T_fuel_max_up = data.httemp555005001;
+
+T_clad_max_do = data.httemp555000108;
+T_clad_max_med = data.httemp555002508;
+T_clad_max_up = data.httemp555005008;
+
+T_profile = data(:,{'httemp555002501', ...
+                    'httemp555002502', ...
+                    'httemp555002503', ...
+                    'httemp555002504', ...
+                    'httemp555002505', ...
+                    'httemp555002506', ...
+                    'httemp555002507', ...
+                    'httemp555002508', ...
+                    'httemp555002509', ...
+                    'httemp555002510'});
+
+p_drop = data.cntrlvar120;
+
+HT_mode_do = floor(data.htmode555000101);
+HT_mode_med = floor(data.htmode555002501);
+HT_mode_up = floor(data.htmode555005001);
+
+
+
+% Creo i plot
+figure(1)
+tiledlayout(3,2)
+
+nexttile
+plot(time,power, 'LineWidth', 1.3, 'Color', 'r');
+title('TOTAL POWER [kW]')
+
+nexttile
+plot(time,T_out, 'LineWidth', 1.3, 'Color', 'r');
+title('OUTLET TEMPERATURE [K]')
+
+nexttile
+hold on
+plot(time,HT_mode_do, 'LineWidth', 1.3, 'Color', 'b');
+plot(time,HT_mode_med, 'LineWidth', 1.3, 'Color', 'g');
+plot(time,HT_mode_up, 'LineWidth', 1.3, 'Color', 'r');
+hold off
+legend('down', 'med', 'up')
+title('HEAT TRANSFER MODE')
+
+
+nexttile
+hold on
+plot(time,T_fuel_max_do, 'LineWidth', 1.3, 'Color', 'b');
+plot(time,T_fuel_max_med, 'LineWidth', 1.3, 'Color', 'g');
+plot(time,T_fuel_max_up, 'LineWidth', 1.3, 'Color', 'r');
+hold off
+legend('down', 'med', 'up')
+title('MAX FUEL TEMPERATURE [K]')
+
+nexttile
+hold on
+plot(time,T_clad_max_do, 'LineWidth', 1.3, 'Color', 'b');
+plot(time,T_clad_max_med, 'LineWidth', 1.3, 'Color', 'g');
+plot(time,T_clad_max_up, 'LineWidth', 1.3, 'Color', 'r');
+hold off
+legend('down', 'med', 'up')
+title('MAX CLAD TEMPERATURE [K]')
+
--- Modelli/PWR/input_strip.i
+++ Modelli/PWR/input_strip.i
@@ -4,19 +4,38 @@
 103 0
 
 
-1001 tempf 555010000
-1002 tempf 555250000
-1003 tempf 555500000
+1001 cntrlvar 110                       $ Potenza Totale
 *
-1004 htvat 5550001
-1005 htvat 5550025
-1006 htvat 5550050
+1002 tempf 555500000                    $ Temperatura in uscita
 *
-1007 httemp 555000101
-1008 httemp 555002501
-1009 httemp 555005001
+1003 httemp 555000101                   $ Temperatura max fuel pellet (low, medium, high)
+1004 httemp 555002501
+1005 httemp 555005001
 *
-1013 cntrlvar 110
+1006 httemp 555000108                   $ Temperatura max cladding (low, medium, high)
+1007 httemp 555002508
+1008 httemp 555005008
+*
+1009 httemp 555002501                   $ Profilo temperatura rod (volume 25)
+1010 httemp 555002502
+1011 httemp 555002503
+1012 httemp 555002504
+1013 httemp 555002505
+1014 httemp 555002506
+1015 httemp 555002507
+1016 httemp 555002508
+1017 httemp 555002509
+1018 httemp 555002510
+*
+1019 cntrlvar 120                       $ Caduta di pressione
+*
+1020 htmode 555000101                   $ Heat transfer mode (low, medium, high)
+1021 htmode 555002501
+1022 htmode 555005001
+*
+1023 htchfr 555000101                   $ CHFR (low, medium, high)
+1024 htchfr 555002501
+1025 htchfr 555005001
 *
 *
 .
