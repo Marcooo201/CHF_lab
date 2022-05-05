a2a91aaa67a1a538613091e67b5aa7ad0697d18e
Lorenzo Mazzocco <lorenzo.mazzocco01@gmail.com>
2022-05-05 10:38:38 +0200

    update

--- Steady State/Sensitivity Analysis/PWR/modifica.py
+++ Steady State/Sensitivity Analysis/PWR/modifica.py
@@ -3,26 +3,8 @@ sys.path.append('../../../utils/other/')
 from python_RELAP_functions import *
 sys.path.append('../../"Steady State"/"Sensitivity Analysis"/PWR')
 
-## Voglio provare a modificare il mass flow rate imposto al canale
-r_file = open("prova_input.txt", "r")
-list_lines = r_file.readlines()
-print(list_lines)
-print(list_lines[2].split())
-words = list_lines[2].split()
-words[1] = "500"
-words.append("\n")
-list_lines[2] = " ".join(words)
-print(list_lines)
 
-w_file = open("prova_input.txt", "w")
-w_file.writelines(list_lines)
-w_file.close()
-
-
-
-
-
-
+###### COME USARE LA FUNZIONE :)))
 prova_dict = {
         "name" : "pressione",
         "line" : 1,
