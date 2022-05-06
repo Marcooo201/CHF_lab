55b830cd8346ae152d68206649b52ab9086a29f9
Lorenzo Mazzocco <lorenzo.mazzocco01@gmail.com>
2022-05-06 16:25:56 +0200

    update

--- Steady State/Sensitivity Analysis/PWR/sensitivity_analysis.py
+++ Steady State/Sensitivity Analysis/PWR/sensitivity_analysis.py
@@ -14,6 +14,15 @@ extract_command = r"py ..\..\..\utils\other\parser.py"
 #           DEFINIZIONE PARAMETRI            #
 ##############################################
 # pressione, T_in, mass flow rate, power
+nominale = {
+        "name" : "nominale",
+        "line" : 29,
+        "word" : 1,
+        "initial_value" : 0.,
+        "final_value" : 0.,
+        "table_rep" : False
+}
+
 time = {
         "name" : "simulation_time",
         "line" : 37,
@@ -46,7 +55,7 @@ T_in = {
         "line" : 84,
         "word" : 3,
         "initial_value" : 566.25,
-        "final_value" : 567.0,
+        "final_value" : 568.0,
         "table_rep" : True
 }
 
@@ -64,45 +73,59 @@ power = {
         "line" : 392,
         "word" : 2,
         "initial_value" : 66351.88,
-        "final_value" : 67000.0,
+        "final_value" : 68000.0,
         "table_rep" : True
 }
 
-parameters = [p_in, p_out, T_in, m_dot, power]
+parameters = [nominale, T_in, m_dot, power]
 
-################### Faccio una prova con T_in
-# Pulisco la cartella
-os.system("mkdir out")
-os.system("del input.i")
-os.system(r"del out\output")
-os.system(r"del out\output_strip")
-os.system(r"del out\rstplt")
-os.system(r"del out\stripf")
+###### OUTPUT ######
+max_fuel_temperature = {}
+max_clad_temperature = {}
 
 
-# Copio il file di input e modifico il tempo di simulazione (riduco a 40s per ottimizzare)
-os.system(copy_input_command)
-modify_RELAP_parameter(time, "input.i")
+##############################################
+#            MODIFICA PARAMETRI              #
+##############################################
 
-# Modifico il parametro di riferimento
-modify_RELAP_parameter(T_in, "input.i")
+for parameter in parameters:
+    # Pulisco la cartella
+    os.system("mkdir out")
+    while len(os.listdir('out/')) != 0:
+        os.system("del input.i")
+        os.system(r"del out\output")
+        os.system(r"del out\output_strip")
+        os.system(r"del out\rstplt")
+        os.system(r"del out\stripf")
+        os.system(r"del out\data.csv")
 
-# Eseguo simulazione relap
-os.system(simulate_command)
-os.system("del read_steam_comment.o")
+    # Copio il file di input e modifico il tempo di simulazione (riduco a 40s per ottimizzare)
+    os.system(copy_input_command)
+    modify_RELAP_parameter(time, "input.i")
 
-# Estraggo i dati
-os.system(r"..\..\..\utils\execution\relap5.exe -i input_strip.i -o out\output_strip -r out\rstplt -s out\stripf")
-os.system(extract_command)
+    # Modifico il parametro di riferimento
+    modify_RELAP_parameter(parameter, "input.i")
+    os.system("copy input.i input_{}.i".format(parameter['name']))
 
-# Leggo i dati
-data = np.genfromtxt("out\data.csv", delimiter=';')
-data = data[-1, :] # seleziono gli elementi dell'ultima riga (risultati steady state)
+    # Eseguo simulazione relap
+    os.system(simulate_command)
+    os.system("del read_steam_comment.o")
 
-fuel_temp = data[1:51]
-clad_temp = data[51:101]
-max_fuel = max(fuel_temp)
-max_clad = max(clad_temp)
-print(max_fuel)
-print(max_clad)
-#os.system("del screen")
+    # Estraggo i dati
+    os.system(r"..\..\..\utils\execution\relap5.exe -i input_strip.i -o out\output_strip -r out\rstplt -s out\stripf")
+    os.system(extract_command)
+
+    # Leggo i dati
+    data = np.genfromtxt("out\data.csv", delimiter=';')
+    data = data[-1, :] # seleziono gli elementi dell'ultima riga (risultati steady state)
+
+    fuel_temp = data[1:51]
+    clad_temp = data[51:101]
+    max_fuel = max(fuel_temp)
+    max_clad = max(clad_temp)
+
+    max_fuel_temperature[parameter['name']] = max_fuel
+    max_clad_temperature[parameter['name']] = max_clad
+
+    print(max_fuel_temperature)
+    print(max_clad_temperature)
