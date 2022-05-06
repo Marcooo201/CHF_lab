ce222cdfe34199321c5f208ecece7dcc6988a4a4
Lorenzo Mazzocco <lorenzo.mazzocco01@gmail.com>
2022-05-06 17:54:46 +0200

    finito la sensitivity analysis (senza CHFR e parte di matlab)

--- Steady State/Sensitivity Analysis/PWR/sensitivity_analysis.py
+++ Steady State/Sensitivity Analysis/PWR/sensitivity_analysis.py
@@ -10,6 +10,8 @@ copy_input_command = r"copy ..\..\Modelli\PWR\input.i input.i"
 simulate_command = r"..\..\..\utils\execution\relap5.exe -i input.i -o out\output -r out\rstplt -Z ..\..\..\utils\execution\tpfh2onew"
 extract_command = r"py ..\..\..\utils\other\parser.py"
 
+
+
 ##############################################
 #           DEFINIZIONE PARAMETRI            #
 ##############################################
@@ -86,7 +88,6 @@ max_clad_temperature = {}
 
 
 
-
 ##############################################
 #            MODIFICA PARAMETRI              #
 ##############################################
@@ -131,22 +132,80 @@ for parameter in parameters:
     max_clad_temperature[parameter['name']] = max_clad
 
 
-print(max_fuel_temperature)
-print(max_clad_temperature)
+# ESEGUO PER LA PRESSIONE
+# Pulisco la cartella
+os.system("mkdir out")
+while len(os.listdir('out/')) != 0:
+    os.system("del input.i")
+    os.system(r"del out\output")
+    os.system(r"del out\output_strip")
+    os.system(r"del out\rstplt")
+    os.system(r"del out\stripf")
+    os.system(r"del out\data.csv")
+
+# Copio il file di input e modifico il tempo di simulazione (riduco a 40s per ottimizzare)
+os.system(copy_input_command)
+modify_RELAP_parameter(time, "input.i")
+
+# Modifico il parametro di riferimento
+modify_RELAP_parameter(p_in, "input.i")
+modify_RELAP_parameter(p_out, "input.i")
+os.system("copy input.i input_{}.i".format('pressure'))
+
+# Eseguo simulazione relap
+os.system(simulate_command)
+os.system("del read_steam_comment.o")
+
+# Estraggo i dati
+os.system(r"..\..\..\utils\execution\relap5.exe -i input_strip.i -o out\output_strip -r out\rstplt -s out\stripf")
+os.system(extract_command)
+
+# Leggo i dati
+data = np.genfromtxt("out\data.csv", delimiter=';')
+data = data[-1, :] # seleziono gli elementi dell'ultima riga (risultati steady state)
+
+fuel_temp = data[1:51]
+clad_temp = data[51:101]
+max_fuel = max(fuel_temp)
+max_clad = max(clad_temp)
+
+max_fuel_temperature['pressure'] = max_fuel
+max_clad_temperature['pressure'] = max_clad
+
 
 ##############################################
 #          ELABORO E SALVO RISULTATI         #
 ##############################################
 
+parameters.pop() # elimino il parametro nominale
+header = [parameter['name'] for parameter in parameters]
+header.append('pressure')
+header = ';'.join(header)
+
+fuel_data = []
+clad_data = []
 for parameter in parameters:
     delta_input = parameter['final_value'] - parameter['initial_value']
     delta_ft = np.subtract(max_fuel_temperature[parameter['name']], max_fuel_temperature['nominale'])
     delta_ct = np.subtract(max_clad_temperature[parameter['name']], max_clad_temperature['nominale'])
-    max_fuel_temperature[parameter['name']] = np.divide(delta_ft,delta_input)
-    max_clad_temperature[parameter['name']] = np.divide(delta_ct,delta_input)
+    fuel_data = np.append(fuel_data, np.divide(delta_ft,delta_input))
+    clad_data = np.append(clad_data, np.divide(delta_ct,delta_input))
 
-print(max_fuel_temperature)
-print(max_clad_temperature)
+# Eseguo per la pressione
+delta_input = p_in['final_value'] - p_in['initial_value']
+delta_ft = np.subtract(max_fuel_temperature['pressure'], max_fuel_temperature['nominale'])
+delta_ct = np.subtract(max_clad_temperature['pressure'], max_clad_temperature['nominale'])
+fuel_data = np.append(fuel_data, np.divide(delta_ft,delta_input))
+clad_data = np.append(clad_data, np.divide(delta_ct,delta_input))
+
+
+# SALVO IL CSV
+body = np.vstack((fuel_data,clad_data))
+print(header)
+print(body)
+np.savetxt('sensitivity_results.csv', body, delimiter=';', comments='', fmt='%.6e', header=header)
+
+
 
 
 ##############################################
