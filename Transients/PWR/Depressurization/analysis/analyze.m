7ed192e961e6336696c8957bf58d18b8d199f4bf
Lorenzo Mazzocco <lorenzo.mazzocco01@gmail.com>
2022-05-09 16:37:07 +0200

    update

--- Transients/PWR/Depressurization/analysis/analyze.m
+++ Transients/PWR/Depressurization/analysis/analyze.m
@@ -31,26 +31,24 @@ for i=1:length(lambdas)
     data(i).max_clad_temp_bct = table2array(data(i).raw_data(:, 7:9)); % [°K]
     data(i).center_radial_temp_profile = table2array(data(i).raw_data(:, 10:20)); % [°K]
     data(i).max_fuel_temp_axial = table2array(data(i).raw_data(:,22:71)); % [°K]
+    data(i).ht_mode_axial = table2array(data(i).raw_data(:,72:121)); % []
+    data(i).heat_flux_axial = table2array(data(i).raw_data(:,122:171))./1000; % [kW/m2]
+    data(i).htc_axial = table2array(data(i).raw_data(:,172:221))./1000; % [kW/m2/°K]
+    data(i).flow_regimes_axial = table2array(data(i).raw_data(:,222:271));  % []
+    data(i).void_fraction_axial = table2array(data(i).raw_data(:,272:321)); % []
+    data(i).quality_axial = table2array(data(i).raw_data(:,322:371));       % []
+    data(i).enthalpy_f_axial = table2array(data(i).raw_data(:,372:421));    % [J/kg] (entalpia del LIQUIDO SATURO)
+    data(i).pressure_axial = table2array(data(i).raw_data(:,422:471));      % [Pa]
+
+    % CALCOLO CHF_W3 (devo fare un loop per come è implementata la
+    % correlazione (richiede vettori, non matrici)
+    for j=1:length(data(i).time)
+        data(i).CHF_W3 = CHF_W3(data(i).pressure_axial(j,:), data(i).quality_axial(j,:), 0.335, data(i).enthalpy_f_axial(j,:), data(i).heat_flux_axial(j,:), 8.79e-5, 1.17808e-2, 3.876); % [kW/m2]
+    end
+
+    data(i).CHFR = data(i).CHF_W3./data(i).heat_flux_axial;
+
 end
-
-% ---- TOTAL POWER -----
-figure('Position', [10 10 900 900])
-hold on
-plot(data(1).time,data(1).power, 'LineWidth', 1.3);
-plot(data(2).time,data(2).power, 'LineWidth', 1.3);
-plot(data(3).time,data(3).power, 'LineWidth', 1.3);
-plot(data(4).time,data(4).power, 'LineWidth', 1.3);
-grid on, grid minor
-xlabel('Time [s]')
-title('TOTAL POWER EXCHANGED [kW]')
-
-
-% ----- ANIMATION PROFILE MAX TEMP FUEL -------
-% figure('Position', [10 10 300 900])
-% axial_plot(horzcat(data(1).time, data(1).max_fuel_temp_axial), true, 'Animation Profile - Max Fuel Temperature', 'Fuel Temperature [K]', 3000)
-
-
-
 
 
 
